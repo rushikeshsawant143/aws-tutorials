0935286b12f3807232e7db662f9c6121e8808770
rushikeshsawant143 <65754477+rushikeshsawant143@users.noreply.github.com>
2021-09-18 12:51:52 +0530

    Update server_start.sh

--- awspipelinedemo/scripts/server_start.sh
+++ awspipelinedemo/scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /home/ec2-user/tomcat/apache-tomcat-8.5.61/bin/./startup.sh
-sudo /home/ec2-user/tomcat/apache-tomcat-8.5.61/bin/./startup.sh
+sudo chmod +x /home/ec2-user/tomcat/apache-tomcat-9.0.35/bin/./startup.sh
+sudo /home/ec2-user/tomcat/apache-tomcat-9.0.35/bin/./startup.sh
