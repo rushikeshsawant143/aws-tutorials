1a69e8abd4598c6c23db4d489637a3aea75efd3f
rushikeshsawant143 <65754477+rushikeshsawant143@users.noreply.github.com>
2021-09-18 12:52:18 +0530

    Update server_stop.sh

--- awspipelinedemo/scripts/server_stop.sh
+++ awspipelinedemo/scripts/server_stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /home/ec2-user/tomcat/apache-tomcat-8.5.61/bin/./shutdown.sh
-sudo /home/ec2-user/tomcat/apache-tomcat-8.5.61/bin/./shutdown.sh
+sudo chmod +x /home/ec2-user/tomcat/apache-tomcat-9.0.35/bin/./shutdown.sh
+sudo /home/ec2-user/tomcat/apache-tomcat-9.0.35/bin/./shutdown.sh
